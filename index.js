c1e7ad40ca10f64a2e4df6bdf74ead27bb7cdb0a
Katei <kateievans28@gmail.com>
2025-03-20 22:36:20 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const LeastFavoriteCustomer = "new"
+
+// function changeLeastFavoriteCustomer() {
+//     return LeastFavoriteCustomer = "nice try"
+// }
+
+// tried arrow functions to practice
+changeLeastFavoriteCustomer = () => LeastFavoriteCustomer = "nice try"; 
